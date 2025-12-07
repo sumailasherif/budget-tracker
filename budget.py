9263d3e78fb220937e7a5a9ad4bda6cacaee29f1
Sherif <sumailasherif773@gmail.com>
2025-12-07 10:34:00 +0400

    added transaction class

---
+++ budget.py
@@ -0,0 +1,7 @@
+class Transaction:
+    def __init__(self, date, amount, category, description, ttype):
+        self.date = date
+        self.amount = amount
+        self.category = category
+        self.description = description
+        self.ttype = ttype   # "income" or "expense"
